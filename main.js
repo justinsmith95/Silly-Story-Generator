0198e951361416e74ccd003e70328bebd519487f
jtsmith9562 <jtsmith9562@gmail.com>
2021-09-16 14:50:04 -0400

    necessary files added

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ newStory = newStory.replace(':insertx', xItem)
   }
 
   if(document.getElementById("uk").checked) {
-    var weight = Math.round(300/14) + 'stone';
+    var weight = Math.round(300/14) + ' stone';
     var temperature =  Math.round((94-32)*5/9) + 'centigrade';
     newStory = newStory.replace('300 pounds', weight);
     newStory = newStory.replace('94 farenheit', temperature);
